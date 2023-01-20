acb153c7747ab4a3672da890333f7b13bf928a1a
Silas Morales <silas.mora22@gmail.com>
2023-01-20 13:09:04 -0800

    exercise complete

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const searchButton = document.getElementById("submitSearch")
+const searchInput = document.getElementById("searchWord")
+const imgGif = document.querySelector("#gif")
+const feedBackele = document.getElementById("feedback")
+
+searchButton.addEventListener("click", (Event) => {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Uqe6q2p7Gxk0rgD1FY7Hu7hRxfj8bQBo&s=${searchInput.value}`)
+
+  .then((res) => {console.log(res) 
+    return res.json()
+  })
+
+  .then((parsed) => {
+    imgGif.src = parsed.data.images.original.url
+  })
+
+  .catch((err) => {
+    console.error(err)
+    feedBackele.textContent = err.message
+  })
+})
